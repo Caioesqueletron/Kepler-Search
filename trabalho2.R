fe7161f917f80c33ed108995785593bbd93c59ac
Caioesqueletron <caiocaio620@gmail.com>
2022-06-27 12:30:45 -0300

    Upload de arquivos

--- trabalho2.R
+++ trabalho2.R
@@ -12,8 +12,6 @@ data.column.class = data$koi_pdisposition
 data$rowid<- NULL
 data$kepid<- NULL
 data$kepler_name<- NULL
-data$koi_disposition<- NULL
-data$koi_pdisposition<- NULL
 data$kepoi_name<-NULL
 data$koi_teq_err1<-NULL
 data$koi_teq_err2<-NULL
@@ -21,29 +19,42 @@ data$koi_teq_err2<-NULL
 
 sapply(data, typeof)
 
+summary(data)
+
 # 4 - Exploração dos dados atraves de medidas de localidade
 
 #função para coletar a moda de atributos nominais
 getmode <- function(v) {
   uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
+ print( uniqv[which.max(tabulate(match(v, uniqv)))])
 
 }
 
-#Medida de distriuição do atributo koi_disposition
-modaKoi_disposition <- getmode(data$koi_disposition)
-print(modaKoi_disposition)
+getLocationsMeasures <- function(value){
+  mean <- mean(value, na.rm = TRUE)
+  print(mean)
+  median <- median(value, na.rm = TRUE)
+  print(median)
+}
 
-#Medida de distriuição do atributo koi_disposition
-modaKoi_pdisposition <- getmode(data$koi_pdisposition)
-print(modaKoi_pdisposition)
+#Medida de localidade do atributo koi_disposition
+result <- getmode(data$koi_disposition)
 
-#Medida de distribuição da koi_pdispodition
-moda2 <- getmode(data$ko)
+#Medida de localidade do atributo koi_disposition
+result <- getmode(data$koi_pdisposition)
+
+#Medida de localidade do atributo koi_score
+result <- getLocationsMeasures(data$koi_score)
+
+#Medida de distribuição da koi_fpflag_nt
+result <- getmode(data$koi_fpflag_nt)
+
+#Medida de distribuição da koi_fpflag_nt
+
 
 
 #Item  7 - Separação de conjuntos de teste e treino
-sample <- sample(c(rep(0, 0.8 * nrow(data)),  # Create dummy for splitting
+sample <- sample(c(rep(0, 0.8 * nrow(data)),  
                    rep(1, 0.2 * nrow(data))))
 table(sample)
 train <- data[sample == 0, ]
